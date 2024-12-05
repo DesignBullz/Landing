2d98a5eb8a74086039fffdeb2bef29a8327edc10
jasmeetsingh23 <js2365536@gmail.com>
2024-12-05 13:27:50 +0530

    updated1234

--- components/Logo.js
+++ components/Logo.js
@@ -1,15 +1,34 @@
+// import React from "react";
+
+// const Logo = () => {
+//   return (
+//     <div className="font-bold flex text-2xl">
+//       {/* <p>Psycho</p>
+//       <p className='text-[#B75CFF]'>Art</p>
+//        */}
+//       <img
+//         src="https://www.csdgaadi.com//alldata/CreateWebsite/Images/images243738704.png"
+//         alt="logo"
+//         className="w-20 h-20"
+//       />
+//     </div>
+//   );
+// };
+
+// export default Logo;
+
 import React from "react";
+import Image from "next/image"; // Import Image from Next.js
 
 const Logo = () => {
   return (
     <div className="font-bold flex text-2xl">
-      {/* <p>Psycho</p>
-      <p className='text-[#B75CFF]'>Art</p>
-       */}
-      <img
+      <Image
         src="https://www.csdgaadi.com//alldata/CreateWebsite/Images/images243738704.png"
         alt="logo"
-        className="w-20 h-20"
+        width={80} // Set width for the image
+        height={80} // Set height for the image
+        className="w-20 h-20" // Optional, can still apply custom classes
       />
     </div>
   );
--- next.config.js
+++ next.config.js
@@ -1,19 +1,42 @@
+// // module.exports = {
+// //   reactStrictMode: true,
+// // }
+
 // module.exports = {
-//   reactStrictMode: true,
-// }
+//   reactStrictMode: true, // Keep reactStrictMode as is
+//   webpack(config, { isServer }) {
+//     // Add video file loader for Webpack
+//     config.module.rules.push({
+//       test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)$/,
+//       use: {
+//         loader: "file-loader",
+//         options: {
+//           publicPath: "/_next/static/videos/",
+//           outputPath: "static/videos/",
+//           name: "[name]-[hash].[ext]",
+//         },
+//       },
+//     });
+
+//     return config;
+//   },
+// };
 
 module.exports = {
   reactStrictMode: true, // Keep reactStrictMode as is
+  images: {
+    domains: ["www.csdgaadi.com"], // Allow external image domains here
+  },
   webpack(config, { isServer }) {
     // Add video file loader for Webpack
     config.module.rules.push({
-      test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)$/,
+      test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)$/, // Add media types you want to handle
       use: {
         loader: "file-loader",
         options: {
-          publicPath: "/_next/static/videos/",
-          outputPath: "static/videos/",
-          name: "[name]-[hash].[ext]",
+          publicPath: "/_next/static/videos/", // Public URL path where videos will be served
+          outputPath: "static/videos/", // Path where videos will be saved in the build output
+          name: "[name]-[hash].[ext]", // Naming convention for the files
         },
       },
     });
